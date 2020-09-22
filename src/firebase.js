138cb8865748508c82f8425432236f4d3beba52c
이승진 <dltmdwls154@gmail.com>
2020-09-22 23:42:36 +0900

    test

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBJU6MmddOEkOP560vFLwhKFVtVz1EzMEA",
-    authDomain: "nwitter-1bba1.firebaseapp.com",
-    databaseURL: "https://nwitter-1bba1.firebaseio.com",
-    projectId: "nwitter-1bba1",
-    storageBucket: "nwitter-1bba1.appspot.com",
-    messagingSenderId: "556543978252",
-    appId: "1:556543978252:web:5fb7e6a2a4560867e5b729",
+    apiKey: "AIzaSyBHZBRzCi5puLObbQx1cdY7bvQ8qFbEaMw",
+    authDomain: "nwitter-47d0b.firebaseapp.com",
+    databaseURL: "https://nwitter-47d0b.firebaseio.com",
+    projectId: "nwitter-47d0b",
+    storageBucket: "nwitter-47d0b.appspot.com",
+    messagingSenderId: "152723115703",
+    appId: "1:152723115703:web:4c48253f0db2ff45718732"
 };
 
 export default firebase.initializeApp(firebaseConfig);
