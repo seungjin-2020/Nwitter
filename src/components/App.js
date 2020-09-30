6dfc1b7c20edd47587596ef18a1caf517f2b33c5
이승진 <dltmdwls154@gmail.com>
2020-09-30 18:45:30 +0900

    Navigation push

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Router } from 'react-router-dom';
 import AppRouter from "components/Router"
 import {authService} from "myBase";
 
+
+
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLoggedIn(true);
+      }else {
+        setIsLoggedIn(false);
+      }
+        setInit(true);
+    });
+  }, [])
+  setInterval(() => {
+    console.log(authService.currentUser)
+  },2000)
   return <>
-    <AppRouter isLoggedIn={isLoggedIn} />;
+    {init ?<AppRouter isLoggedIn={isLoggedIn} /> : "Initalizing......"}
     <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
   </>
   
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,38 @@
-import React from 'react';
+import { dbService } from 'myBase';
+import React, { useState } from 'react';
 
-const Home =() => <span>Home</span>;
+const Home =() => {
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        dbService.collection("newwts").add({
+            nweet,
+            createdAt: Date.now(),
+
+
+        });
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const { 
+            target: {value},
+    } = event;
+        setNweet(value);
+    };
+    return(
+    <div>
+        <form onSubmit={onSubmit}>
+            <input
+                value={nweet}
+                onChange={onChange}
+                type="text"
+                placeholder="What's on your mind?"
+                maxLength={120} />
+            <input type="submit" value="Nweet" />
+        </form>
+    </div>
+    );
+}
+
 export default Home;
